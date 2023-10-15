fc51747f557cfadd2c40fa48001a4f711f9bc014
juanpaRdeCh <juanpablo.ruiz@live.com>
2023-10-15 15:29:37 -0600

    respaldo no funcional

--- NewMain/frontend/src/registrarse.js
+++ NewMain/frontend/src/registrarse.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./registrarse.css";
 
 const Registrarse = () => {
   const [datos, setDatos] = useState({
@@ -30,49 +31,72 @@ const Registrarse = () => {
       throw new Error("No se pudo registrar el usuario");
     }
   };
-
   return (
-    <div>
-      <h2>Registro de nuevos usuarios</h2>
-      <form>
-        <div>
-          <label htmlFor="username">Usuario: </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={datos.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={datos.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="fullName">Nombre Completo: </label>
-          <input
-            type="text"
-            id="fullName"
-            name="fullName"
-            value={datos.fullName}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type="button" onClick={handleSendData}>
-            Crear Usuario
-          </button>
-        </div>
-      </form>
+    <div className="centered-container">
+      <div className="registro-container">
+        <h2>Registro de nuevos usuarios</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Usuario: </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={datos.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña: </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={datos.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="fullName">Nombre Completo: </label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              value={datos.fullName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="aula">Aula: </label>
+            <input
+              type="text"
+              id="aula"
+              name="aula"
+              value={datos.aula}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="permissions">Permisos: </label>
+            <select
+              id="permissions"
+              name="permissions"
+              value={datos.permissions}
+              onChange={handleChange}
+            >
+              <option value="coordinador de aula">Coordinador de Aula</option>
+              <option value="coordinador nacional">Coordinador Nacional</option>
+              <option value="coordinador regional">Coordinador Regional</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <button type="button" onClick={handleSendData}>
+              Crear Usuario
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
-
 export default Registrarse;
