9819fce2e207f22c366c1548130d4e1ff5af2359
val-mtz <A01782413@tec.mx>
2023-10-17 19:39:30 -0600

    Added changes to dark theme and myappbar component

--- NewMain/frontend/src/App.js
+++ NewMain/frontend/src/App.js
@@ -1,4 +1,49 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material/styles"; // Import ThemeProvider
+import { Route } from "react-router-dom";
+import darkTheme from "./theme";
+//import lightTheme from "./lightTheme"; // Import your light theme configuration
+import { Admin, Resource, CustomRoutes, Layout } from "react-admin";
+import { MyAppBar } from "./MyAppBar";
+import { TicketList, TicketEdit, TicketCreate } from "./TicketList";
+import { dataProvider } from "./dataProvider";
+import Registrarse from "./registrarse";
+import authProvider from "./authProvider";
+import MyLoginPage from "./MyLoginPage";
+import { i18nProvider } from "./i18nProvider";
+import Reportes from "./reportes";
+
+const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
+
+const PostIcon = require("@mui/icons-material/Book").default;
+
+const App = () => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Admin
+        loginPage={MyLoginPage}
+        theme={darkTheme}
+        dataProvider={dataProvider}
+        layout={MyLayout}
+        darkTheme={{ palette: { mode: "dark" } }}
+        authProvider={authProvider}
+        i18nProvider={i18nProvider}
+      >
+        <Resource name="tickets" list={TicketList} edit={TicketEdit} create={TicketCreate} />
+        <Resource name="reportes" options={{ label: "Reportes" }} list={Reportes} icon={PostIcon} />
+
+        <CustomRoutes>
+          <Route path="/registrarse" element={<Registrarse />} />
+        </CustomRoutes>
+      </Admin>
+    </ThemeProvider>
+  );
+};
+
+export default App;
+
+/*
+import React from "react";
 import { Route } from "react-router-dom";
 import darkTheme from "./theme";
 import { Admin, Resource, CustomRoutes, Layout } from "react-admin";
@@ -49,6 +94,7 @@ const App = () => {
   );
 };
 export default App;
+*/
 
 // import React from "react";
 // import { Route } from "react-router-dom";
--- NewMain/frontend/src/MyAppBar.js
+++ NewMain/frontend/src/MyAppBar.js
@@ -1,3 +1,19 @@
+import React from "react";
+import { AppBar } from "react-admin";
+import { useTheme } from "@mui/material/styles";
+
+export const MyAppBar = () => {
+  const theme = useTheme();
+
+  const appBarColor = theme.palette.mode === "dark" ? "#a80808" : "#ff0000";
+
+  return (
+  <AppBar style={{ backgroundColor: appBarColor }} />
+  );
+};
+
+/*
 import { AppBar, ToggleThemeButton } from "react-admin";
 
 export const MyAppBar = () => <AppBar toolbar={<ToggleThemeButton />} />;
+*/
